5d906e0309dc3f3d1559adeefa9f197916b5743c
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-03-17 17:17:09 +0330

    [ADD] Cart modal

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -30,20 +30,6 @@ export default function TabLayout() {
                     headerShown: false,
                     title: "Menu",
                     tabBarIcon: ({ color }) => <TabBarIcon name="cutlery" color={color} />,
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({ pressed }) => (
-                                    <FontAwesome
-                                        name="info-circle"
-                                        size={20}
-                                        color={Colors.light.text}
-                                        style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                                    />
-                                )}
-                            </Pressable>
-                        </Link>
-                    ),
                 }}
             />
             <Tabs.Screen
